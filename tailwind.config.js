081bf8a5b0def79f04ad4df8015b83a7254c3cf6
monjuAflame <monjuaflame@gmail.com>
2021-09-27 10:56:45 +0600

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "theme-purple": "#5267DF",
+        "theme-red": "#FA5959",
+        "theme-blue": "#242A45",
+        "theme-grey": "#9194A2",
+        "theme-white": "#f7f7f7",
+        "theme-primary": "#fd3d57",
+        "theme-secondary": "#2b2d42",
+      },
+      fontFamily:{
+        Poppin: ["Poppins, sans-serif"],
+        Roboto: ["Roboto, sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          lg : "1024px",
+          xl : "1124px",
+          "2xl" : "1124px",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      visibility:['group-hover']
+    },
+  },
+  plugins: [],
+}
